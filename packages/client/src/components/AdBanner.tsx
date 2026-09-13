2580dfd34b08e2559541b2589f12ae45e14d60ec
aboredloner <mertisyoooo@gmail.com>
2026-09-13 12:41:03 +0300

    adbanner change.

--- packages/client/src/components/AdBanner.tsx
+++ packages/client/src/components/AdBanner.tsx
@@ -24,7 +24,7 @@ export function AdBanner() {
         </a>
 
         <a 
-          href="https://www.twitch.tv/hun1ress" 
+          href="https://www.twitch.tv/ochidotv" 
           target="_blank" 
           rel="noopener noreferrer"
           className="flex-1 flex flex-col items-center justify-center p-3 rounded-lg bg-bg-hover hover:bg-white/10 transition-colors group cursor-pointer"
